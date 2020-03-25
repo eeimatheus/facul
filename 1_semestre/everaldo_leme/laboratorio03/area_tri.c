44ff46ea0ccfba8324206c669982da2312de8fc0
Matheus <matheusmirandabj@gmail.com>
2020-03-25 17:02:26 -0300

    testes de branch on vuma

--- 1_semestre/everaldo_leme/laboratorio03/area_tri.c
+++ 1_semestre/everaldo_leme/laboratorio03/area_tri.c
@@ -8,7 +8,7 @@ int main()
 	scanf ("%f", &base);
 	printf("Altura do Triangulo: ");
 	scanf ("%f", &alt);
-	area= (base * alt)/2;
+	area= (base * alt)/2;//vuma
 
     area>=1000 ? printf("Triangulao bixo: %.2f. \n", area) :  printf("Area total do Triangulo:  %.2f. \n", area);
 
